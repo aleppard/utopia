e2c9f48801073f4175fddfeeb448b681ef462b86
Andrew Leppard <andrew@leppard.id.au>
2022-09-19 14:18:53 +0930

    Set defaults, added @todo.

--- src/main/java/com/utopia/Map.java
+++ src/main/java/com/utopia/Map.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 /**
  *
+ * @todo Rename to OutputMap.
  */
 public class Map
 {
     // @todo Use Region class here.
-    public int startX;
-    public int startY;
-    public int width;
-    public int height;
-    public List<Integer>[][] tiles;
+    public int startX = 0;
+    public int startY = 0;
+    public int width = 0;
+    public int height = 0;
+    public List<Integer>[][] tiles = null;
 
     // @todo We serialise this as a number (0/1) rather than a boolean
     // (true/false) to save space. The following annotation does not seem
@@ -21,7 +22,7 @@ public class Map
     // @JsonFormat(shape = JsonFormat.Shape.NUMBER)
     // A better solution is to move to bson.
     public short[][] isTraverseable;
-
+    
     public Map(int startX, int startY, int width, int height) {
         this.startX = startX;
         this.startY = startY;
