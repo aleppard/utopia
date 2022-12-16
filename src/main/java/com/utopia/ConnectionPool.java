b2041d02d013c1f7b9746d278a55622d0f7efd79
Andrew Leppard <andrew@leppard.id.au>
2022-12-17 08:43:33 +1030

    Added option to override database configuration using environment variables for Docker.

--- src/main/java/com/utopia/ConnectionPool.java
+++ src/main/java/com/utopia/ConnectionPool.java
@@ -1,7 +1,11 @@
 package com.utopia;
 
+import java.io.InputStream;
+
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import java.util.Properties;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,7 +33,52 @@ public class ConnectionPool {
             // dataSource.databaseName=utopia
             // dataSource.portNumber=5432
             // dataSource.serverName=127.0.0.1
-            config = new HikariConfig("/properties/hikari.properties");
+            Properties properties = new Properties();
+            InputStream input =
+                ConnectionPool.class.getClassLoader().getResourceAsStream
+                ("properties/hikari.properties");
+            properties.load(input);
+            
+            // Support overloading values from configuration through
+            // environment variables. This is useful for configuring
+            // the application when running under Docker compose.
+            {
+                final String user = System.getenv("UTOPIA_DATABASE_USER");
+                if (user != null) {
+                    properties.setProperty("dataSource.user", user);
+                }
+            }
+
+            {
+                final String password =
+                    System.getenv("UTOPIA_DATABASE_PASSWORD");
+                if (password != null) {
+                    properties.setProperty("dataSource.password", password);
+                }
+            }
+                    
+            {
+                final String name = System.getenv("UTOPIA_DATABASE_NAME");
+                if (name != null) {
+                    properties.setProperty("dataSource.databaseName", name);
+                }
+            }
+
+            {
+                final String port = System.getenv("UTOPIA_DATABASE_PORT");
+                if (port != null) {
+                    properties.setProperty("dataSource.portNumber", port);
+                }
+            }
+
+            {
+                final String host = System.getenv("UTOPIA_DATABASE_HOST");
+                if (host != null) {
+                    properties.setProperty("dataSource.serverName", host);
+                }
+            }
+            
+            config = new HikariConfig(properties);
             datasource = new HikariDataSource(config);
         }
         catch (Exception exception) {
